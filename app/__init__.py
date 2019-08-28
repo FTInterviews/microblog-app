40a89d4da7826d775ec3ebb926520362b1892d28
Ameen Rashad <ameenrashadv@gmail.com>
2019-08-28 22:57:30 +0530

    Added login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,8 @@
-from flask import render_template
-from app import app
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+from app import routes
